e3ad2c11680ec34a23bacecc1388b5f4ed391468
Lloyd Africa <lloydafrica@gmail.com>
2023-07-31 00:57:29 +0200

    deploy script update

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://themeticus.github.io
-git push -f git@github.com:themeticus/themeticus.github.io.git main
+# git push -f git@github.com:themeticus/themeticus.github.io.git main
 
 # if you are deploying to https://themeticus.github.io/<REPO>
 git push -f git@github.com:themeticus/order-tracker-component-vue.git main:gh-pages
